2fed838bd621a88e0a7ef9322f26485e1fe2b9a8
bmalkes <bmalkes@gmail.com>
2023-08-16 19:54:35 +0300

    feat:add context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useData } from "../../Context/DataContext";
 
 export const Header = () => {
+  const { data } = useData();
+  console.log(data);
   return <div>Header</div>;
 };
